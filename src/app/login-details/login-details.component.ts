4a9808d6958a8312d754dc40e484eb8cfc5b6ca6
sudheer <sudheerveeranki97@gmail.com>
2022-03-16 13:20:51 -0400

    initial commit

---
+++ src/app/login-details/login-details.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-details',
+  templateUrl: './login-details.component.html',
+  styleUrls: ['./login-details.component.css']
+})
+export class LoginDetailsComponent implements OnInit {
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+}
+
